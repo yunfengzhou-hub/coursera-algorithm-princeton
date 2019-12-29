1d4be5a5486cd940a9dd8bad500f454f8b05198f
yunfeng zhou <yunfengz@andrew.cmu.edu>
2019-12-28 22:50:28 -0500

    draft for percolation

---
+++ percolation/GridRoot.java
@@ -0,0 +1,43 @@
+package percolation;
+
+public class GridRoot{
+    private static Integer[][][] gridRoot;
+    public GridRoot(int n){
+        gridRoot = new Integer[n][n][2];
+        for(Integer i=0;i<n;i++){
+            for(Integer j=0;i<n;i++){
+                gridRoot[i][j][0] = i;
+                gridRoot[i][j][1] = j;
+            }
+        }
+    }
+    public Integer[] getRoot(int row, int col){
+        Integer[] rootIndex = new Integer[2];
+        rootIndex[0] = col - 1;
+        rootIndex[1] = row - 1;
+        while(gridRoot[rootIndex[0]][rootIndex[1]][0] != rootIndex[0] || 
+        gridRoot[rootIndex[0]][rootIndex[1]][1] != rootIndex[1]){
+            rootIndex[0] = gridRoot[rootIndex[0]][rootIndex[1]][0];
+            rootIndex[1] = gridRoot[rootIndex[0]][rootIndex[1]][1];
+        }
+        return rootIndex;
+    }
+    public void setRoot(int row, int col, Integer[] newIndex){
+        Integer[] oldIndex = getRoot(row,col);
+        gridRoot[oldIndex[0]][oldIndex[1]][0] = newIndex[0];
+        gridRoot[oldIndex[0]][oldIndex[1]][1] = newIndex[1];
+    }
+    public boolean equalRoot(int row1, int col1, int row2, int col2){
+        Integer[] root1 = getRoot(row1,col1);
+        Integer[] root2 = getRoot(row2,col2);
+        return root1[0]==root2[0] && root1[1]==root2[1];
+    }
+    // following function could be changed according to different
+    // union-find strategy.
+    public void mergeRoot(int row1, int col1, int row2, int col2){
+        if(equalRoot(row1, col1, row2, col2)){
+            return;
+        }
+        setRoot(row2, col2, getRoot(row1,col1));
+    }
+}
--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,23 +1,100 @@
+package percolation;
+
 public class Percolation {
+    private static Boolean[][] gridOpen;
+    private static Boolean[][] gridFull;
+    private static Boolean isPercolate;
+    private static Integer numOpen,gridSize;
+    private static Boolean needUpdate;
+    GridRoot gridRoot;
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n)
+    public Percolation(int n){
+        gridOpen = new Boolean[n][n];
+        gridFull = new Boolean[n][n];
+        isPercolate = false;
+        numOpen = 0;
+        gridSize = n;
+        gridRoot = new GridRoot(n);
+        needUpdate = false;
+
+        for(Integer i=0;i<n;i++){
+            for(Integer j=0;i<n;i++){
+                gridOpen[i][j] = false;
+                gridFull[i][j] = false;
+            }
+        }
+    }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col)
+    public void open(int row, int col){
+        if(gridOpen[col-1][row-1]){
+            return;
+        }
+
+        numOpen++;
+        gridOpen[col-1][row-1] = true;
+        needUpdate = true;
+
+        if(row>1 && isOpen(row-1,col)){
+            gridRoot.mergeRoot(row, col, row-1, col);
+        }
+        if(row<gridSize && isOpen(row+1,col)){
+            gridRoot.mergeRoot(row, col, row+1, col);
+        }
+        if(col>1 && isOpen(row,col-1)){
+            gridRoot.mergeRoot(row, col, row, col-1);
+        }
+        if(col<gridSize && isOpen(row,col+1)){
+            gridRoot.mergeRoot(row, col, row, col+1);
+        }
+        return;
+    }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col)
+    public boolean isOpen(int row, int col){
+        return gridOpen[col-1][row-1];
+    }
+
+    private void updateGrid(){
+        if(!needUpdate){
+            return;
+        }
+        Integer[] gridIndex;
+        for(Integer i=0;i<gridSize;i++){
+            if(isOpen(i,gridSize-1)){
+                gridIndex = gridRoot.getRoot(i,gridSize-1);
+                gridFull[gridIndex[0]][gridIndex[1]] = true;
+            }
+        }
+        needUpdate = false;
+    }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col)
+    public boolean isFull(int row, int col){
+        updateGrid();
+        Integer[] gridIndex = gridRoot.getRoot(row,col);
+        return gridFull[gridIndex[0]][gridIndex[1]];
+    }
 
     // returns the number of open sites
-    public int numberOfOpenSites()
+    public int numberOfOpenSites(){
+            return numOpen.intValue();
+    }
 
     // does the system percolate?
-    public boolean percolates()
+    public boolean percolates(){
+        updateGrid();
+        for(Integer i=0;i<gridSize;i++){
+            if(isFull(i,gridSize-1)){
+                return true;
+            }
+        }
+        return false;
+    }
 
     // test client (optional)
-    public static void main(String[] args)
+    public static void main(String[] args){
+
+    }
 }
